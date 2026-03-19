3b063e191a119d1515c3f368620b58d59248ed90
michal92299 <chudybyczek2137@gmail.com>
2026-03-19 20:23:29 +0100

    Create switch.rs

---
+++ source-code/src/commands/switch.rs
@@ -0,0 +1,28 @@
+use anyhow::Result;
+use colored::Colorize;
+use std::fs;
+use std::path::Path;
+use crate::{STORE_PATH, state::State};
+
+pub fn switch_version(package: String, version: String) -> Result<()> {
+    let lock = crate::utils::acquire_lock()?;
+    let _guard = scopeguard::guard(lock, |_| crate::utils::release_lock());
+
+    let state = State::load()?;
+
+    if !state.packages.contains_key(&package) {
+        anyhow::bail!("Package {} not installed", package);
+    }
+    if !state.packages[&package].contains_key(&version) {
+        anyhow::bail!("Version {} of package {} not installed", version, package);
+    }
+
+    let current_link = Path::new(STORE_PATH).join(&package).join("current");
+    if current_link.exists() {
+        fs::remove_file(&current_link)?;
+    }
+    std::os::unix::fs::symlink(&version, &current_link)?;
+
+    println!("{} Switched {} to version {}", "✔".green(), package.cyan(), version.cyan());
+    Ok(())
+}
