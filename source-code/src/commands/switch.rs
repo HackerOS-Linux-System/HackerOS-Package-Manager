fc4ce2871e46ec94b7fafa4d130ae091629397fc
michal92299 <chudybyczek2137@gmail.com>
2026-04-03 14:11:25 +0200

    Update switch.rs

--- source-code/src/commands/switch.rs
+++ source-code/src/commands/switch.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use miette::{Result, IntoDiagnostic};
 use colored::Colorize;
 use std::fs;
 use std::path::Path;
@@ -11,17 +11,17 @@ pub fn switch_version(package: String, version: String) -> Result<()> {
     let state = State::load()?;
 
     if !state.packages.contains_key(&package) {
-        anyhow::bail!("Package {} not installed", package);
+        miette::bail!("Package {} not installed", package);
     }
     if !state.packages[&package].contains_key(&version) {
-        anyhow::bail!("Version {} of package {} not installed", version, package);
+        miette::bail!("Version {} of package {} not installed", version, package);
     }
 
     let current_link = Path::new(STORE_PATH).join(&package).join("current");
     if current_link.exists() {
-        fs::remove_file(&current_link)?;
+        fs::remove_file(&current_link).into_diagnostic()?;
     }
-    std::os::unix::fs::symlink(&version, &current_link)?;
+    std::os::unix::fs::symlink(&version, &current_link).into_diagnostic()?;
 
     println!("{} Switched {} to version {}", "✔".green(), package.cyan(), version.cyan());
     Ok(())
